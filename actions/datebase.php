0932ae084848feec7a48bbd9b3fd7ee8db826a7f
Raphael <guet.raphael@hotmail.com>
2022-11-30 16:56:37 +0100

    Creation du form + connexion à la bdd

---
+++ actions/datebase.php
@@ -0,0 +1,11 @@
+<?php
+
+
+
+try {
+    //permet de se connecter à la bdd
+    $bdd = new PDO('mysql:host=localhost;dbname=forum;charset=utf8;', 'root', 'root');
+
+} catch (Exception $e) {
+    die('Une erreur a été detecté : ' . $e->getMessage());
+}
